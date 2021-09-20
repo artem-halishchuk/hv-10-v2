46beb1af32d378065038b2dce52c40641ebb0caf
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-09-20 20:32:37 +0300

    final ver 1.2

--- script.js
+++ script.js
@@ -1,235 +1,217 @@
-//https://artem-halishchuk.github.io/hw-10/index.html
-document.addEventListener("DOMContentLoaded", function() {
-    class FormAdd {
-        constructor(blockInsert) {
-            this.blockInsert = blockInsert;
-        }
-        createForm() {
-            let form = document.createElement('div');
-            form.classList.add('form');
-            this.blockInsert.append(form);
+document.addEventListener('DOMContentLoaded', function() {
+	let users = [];
 
-            let input = document.createElement('input');
-            input.classList.add('input', 'form-name');
-            form.append(input);
-
-            let buttonAdd = document.createElement('button');
-            buttonAdd.classList.add('button', 'form-add');
-            buttonAdd.innerHTML = 'add';
-            form.append(buttonAdd);
-        }
-    }
-    class ContentList {
-        constructor(blockInsert) {
-            this.blockInsert = blockInsert;
-        }
-        createContentList() {
-            let ul = document.createElement('ul');
-            ul.classList.add('content-list');
-            this.blockInsert.append(ul);
-        }
-    }
-    class ContentItem {
-        constructor(blockInsert, name, index) {
-            this.blockInsert = blockInsert;
-            this.name = name;
-            this.index = index;
-        }
-        createContentItem() {
-            let li = document.createElement('li');
-            li.classList.add('content-item');
-            this.blockInsert.append(li);
-
-            let buttonShow = document.createElement('button');
-            buttonShow.classList.add('content-item-name');
-            buttonShow.innerHTML = this.name;
-            li.append(buttonShow);
-
-            let buttonDelete = document.createElement('button');
-            buttonDelete.classList.add('delete');
-            li.append(buttonDelete);
-
-            li.dataset.index = this.index;
-            return li;
-        }
-    }
-
-    class SearchForm {
-        constructor(blockInsert) {
-            this.blockInsert = blockInsert;
-        }
-        createSearch() {
-            let div = document.createElement('div');
-            div.classList.add('form', 'search');
-            this.blockInsert.append(div);
-            let input = document.createElement('input');
-            input.classList.add('input', 'form-search');
-            div.append(input);
-        }
-    }
-
-    class NoteName {
-        constructor(blockInsert) {
-            this.blockInsert = blockInsert;
-        }
-        createNoteName() {
-            let input = document.createElement('input');
-            input.classList.add('input', 'note-name');
-            this.blockInsert.append(input);
-        }
-    }
-    class NoteForm {
-        constructor(blockInsert) {
-            this.blockInsert = blockInsert;
-        }
-        createNoteForm() {
-            let textarea = document.createElement('textarea');
-            textarea.classList.add('input', 'note-form');
-            this.blockInsert.append(textarea);
-        }
-    }
-
-    //получение menu-main
-    let getMainMenu = document.querySelector('.menu-main');
-    //создание формы
-    let formAddUser = new FormAdd(getMainMenu);
-    formAddUser.createForm();
-    //создание ul для списка
-    let contentListUsers = new ContentList(getMainMenu);
-    contentListUsers.createContentList();
-
-
-    //получение menu-note
-    let getMenuNote = document.querySelector('.menu-note');
-    // создание noteName
-    let noteName = new NoteName(getMenuNote);
-    noteName.createNoteName();
-    // создание noteForm
-    let noteForm = new NoteForm(getMenuNote);
-    noteForm.createNoteForm();
-
-    let users = []; //массив юзеров
     class User {
-        constructor(name, id) {
+        constructor(name) {
             this.name = name;
-            this.id = id;
             this.notes = [];
         }
-    };
+    }
+
+    //test users start
+    let vasia = new User('vasia', 0);
+	let petia = new User('petia', 1);
+	users.push(vasia);
+	users.push(petia);
+	//test users end
+
     class Note {
         constructor(name, note) {
             this.name = name;
             this.note = note;
         }
-    };
-
-
-
-    let click = document.querySelector('.app');
-    //создание user
-    click.addEventListener('click', ()=> addUser(event));
-    function addUser (event) {
-        if(!event.target.matches('.menu-main .form-add')) return;
-        let name = document.querySelector('.menu-main .form-name');
-        if (name.value.trim() === '') {
-            alert('Entered name');
-            return;
-        }
-        users.push(new User(name.value));
-        name.value = null;
-        showUsers(users);
-    };
-    //изменение активного юзера
-    let activeUser;
-    let activeUserIndex;
-    click.addEventListener('click', ()=> changesActiveUser(event));
-    function changesActiveUser(event) {
-        if (!event.target.matches('.content-item-name')) return;
-        let contentList = document.querySelector('.menu-main .content-list');
-        contentList.childNodes.forEach(e => {
-            e.firstChild.classList.remove('content-item-name-active');
-        });
-        event.target.classList.add('content-item-name-active');
-        activeUserIndex = event.target.parentNode.dataset.index;
-        activeUser = users[activeUserIndex];
-        console.log(activeUser);
-        showNotes(activeUserIndex);
     }
 
-    //создание note
-    click.addEventListener('click', ()=> addNote(event, 0));
-    function addNote (event, activeUser) {
-        if (!event.target.matches('.menu-notes .form-add')) return;
-        let name = document.querySelector('.menu-notes .form-name');
-        if (name.value.trim() === '') {
-            alert('Entered name');
-            return;
+    //test notes start
+    let note = new Note('note-1', 'text note-1');
+    let note2 = new Note('note-2', 'text note-2');
+    vasia.notes.push(note);
+    petia.notes.push(note2);
+    //test notes end
+
+	class MenuMain {
+		constructor(blockInsert, arrElem, removeBlock, container) {
+            this.blockInsert = blockInsert;
+            this.arrElem = arrElem;
+            this.activeElement = null;
+            this.ul = null;
+            this.removeBlock = removeBlock;
+            this.container = container;
+        };
+        createMenu() {
+        	let container = document.createElement('div');
+            container.classList.add(this.container);
+            this.blockInsert.append(container);
+
+            let formWrapper = document.createElement('div');
+            formWrapper.classList.add('form');
+            container.append(formWrapper);
+
+            let input = document.createElement('input');
+            input.placeholder = 'Имя пользователя';
+            input.classList.add('input', 'form-name');
+            formWrapper.append(input);
+
+            let buttonAdd = document.createElement('button');
+            buttonAdd.classList.add('button', 'form-add');
+            buttonAdd.innerHTML = 'add';
+            formWrapper.append(buttonAdd);
+            buttonAdd.addEventListener('click', () => {
+                if(input.value.trim() === '') {
+                    alert('Заполните поле имени.');
+                    return;
+                }
+                this.name = input.value;
+                this.createElem();
+                input.value = '';
+                this.show(this.arrElem); //отображение созданого элемента
+            });
+
+            this.ul = document.createElement('ul');
+            this.ul.classList.add('content-list');
+            container.append(this.ul);
+            this.show();
+            this.deactivateElement();
         }
-        users[activeUser].notes.push(new Note(name.value));
-        name.value = null;
-        //showNotes(activeUserIndex);
-    };
-
-    //добавление изеров в список меню main
-    let contentListMain = document.querySelector('.menu-main .content-list');
-    function showUsers(users) {
-        contentListMain.innerHTML = '';
-        let userItem = users.map((element, index) =>
-            new ContentItem(contentListMain, element.name, index).createContentItem());
-        contentListMain.append(...userItem);
-        if (activeUserIndex) {
-            contentListMain.childNodes[activeUserIndex].firstChild.classList.add('content-item-name-active');
+        createElem() {
+            this.arrElem.push(new User(this.name, this.arrElem.length));
         }
+        show() {
+            this.ul.innerHTML = '';
+            if (!this.arrElem) return;
+            this.arrElem.map((e, i) => {
+                let li = document.createElement('li');
+                li.classList.add('content-item');
+                this.ul.append(li);
 
-    };
+                let buttonShow = document.createElement('button');
+                buttonShow.classList.add('content-item-name');
+                buttonShow.innerHTML = e.name;
+                li.append(buttonShow);
+                li.addEventListener('click', (event) => {
+                    if (event.target.matches('.content-item-name-active')) return;                 
+                    this.activeElem(event.target);
+                    if (e === this.activeElement) buttonShow.classList.add('content-item-name-active');
+                })
+                if (e === this.activeElement) buttonShow.classList.add('content-item-name-active');
 
-    //добавление заметок в список меню notes
+                let buttonDelete = document.createElement('button');
+                buttonDelete.classList.add('delete');
+                li.append(buttonDelete);
+                buttonDelete.addEventListener('click', (event) => {
+                    event.stopPropagation();                    
+                    this.deleteElem(event);
+                    this.show();
+                    if(!event.target.parentNode.firstChild.matches('.content-item-name-active')) return;
+                    this.display();
+                })
+                li.dataset.index = i;
+            })
+        }
+        activeElem(event) {
+            if(!event) return;
+            let index = event.parentNode.dataset.index;
+            this.activeElement = this.arrElem[index];
 
-    function showNotes(notes) {
-        let contentListNotes = document.querySelector('.menu-notes .content-list');
-        contentListNotes.innerHTML = '';
-        let noteItem = notes.map((element, index) =>
-            new ContentItem(contentListNotes, element.name, index).createContentItem());
-        contentListNotes.append(...noteItem);
-        // if (activeUserIndex) {
-        //     contentListMain.childNodes[activeUserIndex].firstChild.classList.add('content-item-name-active');
-        // }
+            this.ul.childNodes.forEach(e => {
+                e.firstChild.classList.remove('content-item-name-active');
+                if(e.dataset.index === index) e.firstChild.classList.add('content-item-name-active');
+            });
+            this.show();
+            this.display();
+        }
+        deleteElem(event) {
+            let index = event.target.parentNode.dataset.index;
+            if (this.arrElem[index] === this.activeElement) this.activeElement = null;
+            this.arrElem.splice(event.target.parentNode.dataset.index, 1);
+            if (event.target.parentNode.firstChild.matches('.content-item-name-active')) {
+                if (document.querySelector('.menu-note')) document.querySelector('.menu-note').style.display = 'none';
+            }
+        }
+        display() {
+            if ((!this.activeElement) && document.querySelector(this.removeBlock)) {
+                document.querySelector(this.removeBlock).style.display = 'none';
+            }
+            let menuNotes;
+            if (this.activeElement) {
+                if (document.querySelector(this.removeBlock)) {
+                    document.querySelector(this.removeBlock).remove();
+                }
+                menuNotes = new MenuNotes(getBlockApp, this.activeElement.notes, '.menu-note', 'menu-notes');
+                menuNotes.createMenu();
+                menuNotes.display();
+            }
+        }
+        deactivateElement() {
+            let click = null;
+            document.addEventListener('click', (event) => {
+                click = event.target;
+                if (!click.matches(`.${this.container}`)) return;
+                this.activeElement = null;
+                this.show();
+                this.display();
+                if (document.querySelector('body .menu-note')) {
+                    document.querySelector('.menu-note').style.display = 'none';
+                }
+            })
+        }
+	}
 
-    };
 
-    //отображение заметок
-    //click.addEventListener('click', ()=> showNotes(event, 0));
-    function showNotes(activeUserIndex) {
-        if (!activeUserIndex) return;
-        let menuNotes = document.querySelector('.menu-notes');
-        menuNotes.style.display = 'block';
-        menuNotes.innerHTML = '';
-        //получение menu-notes
-        let getNotesMenu = document.querySelector('.menu-notes');
-        //создание формы
-        let formAddNote = new FormAdd(getNotesMenu);
-        formAddNote.createForm();
-        //создание search
-        let searchForm = new SearchForm(getNotesMenu);
-        searchForm.createSearch();
-        //создание ul для списка
-        let contentListNotes = new ContentList(getNotesMenu);
-        contentListNotes.createContentList();
-
-
-        let contentListNotes1 = document.querySelector('.menu-notes .content-list');
-        contentListNotes1.innerHTML = '';
-        let noteItem = users[activeUser].notes.map((element, index) =>
-            new ContentItem(contentListNotes, element.name, index).createContentItem());
-        contentListNotes1.append(...noteItem);
+    class MenuNotes extends MenuMain {
+        constructor(blockInsert, arrElem, removeBlock, container) {
+            super(blockInsert, arrElem, removeBlock, container);
+        }
+        createElem() {
+            this.arrElem.push(new Note(this.name));
+        }
+        display() {
+            if ((!this.activeElement) && document.querySelector(this.removeBlock)) {
+                document.querySelector(this.removeBlock).style.display = 'none';
+            }
+            let menuNote;
+            if (this.activeElement) {
+                if (document.querySelector(this.removeBlock)) {
+                    document.querySelector(this.removeBlock).remove();
+                }
+                menuNote = new MenuNote(getBlockApp, this.activeElement);
+                menuNote.createMenu();
+            }
+        }
     }
 
 
-    // click.addEventListener('click', ()=> {
-    //     if (!event.target.matches('.menu-main .content-item-name')) return;
-    //     let index = event.target.parentNode.dataset.index;
-    //     activeUser = users[index];
-    // });
+    class MenuNote {
+        constructor(blockInsert, note) {
+            this.blockInsert = blockInsert;
+            this.note = note;
+        }
+        createMenu() {
+            let container = document.createElement('div');
+            container.classList.add('menu-note');
+            this.blockInsert.append(container);
+
+            let input = document.createElement('input');
+            input.classList.add('input', 'note-name');
+            container.append(input);
+            input.value = this.note.name;
+            input.oninput = () => {
+                this.note.name = input.value;
+                document.querySelector('.menu-notes .content-item-name-active').textContent = input.value;
+            }
+
+            let textarea = document.createElement('textarea');
+            textarea.classList.add('input', 'note-form');
+            container.append(textarea);
+            textarea.textContent = this.note.note;
+            textarea.oninput = () => {
+                this.note.note = textarea.value;
+            }
+        }
+    }
 
 
+    let getBlockApp = document.querySelector('.app');
+    let menuMain = new MenuMain(getBlockApp, users, '.menu-notes', 'menu-main');
+    menuMain.createMenu();
 })
